f82a8f83230677d88a717eb9fc689a5ab7a91665
HasanKaps <40915487+HasanKaps@users.noreply.github.com>
2019-10-27 10:15:19 +0400

    updated x2

---
+++ server/routes/main.js
@@ -0,0 +1,12 @@
+module.exports = function(app)
+{
+     app.get('/',function(req,res){
+        res.render('index.html')
+     });
+     app.get('/about',function(req,res){
+        res.render('about.html');
+    });
+     app.get('/leaderboards',function(req,res){
+        res.render('leaderboards.html');
+    });
+}
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-var express    =    require('express');
-var app        =    express();
+const express    =    require('express');
+const bparser = require('body-parser');
+const app        =    express();
 
-require('./route/main')(app);
+require('./routes/main')(app);
 app.set('views',__dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
