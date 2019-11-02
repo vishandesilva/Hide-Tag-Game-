4ab45828f7e4a1633ed8e43ed3511b335df40b48
HasanKaps <40915487+HasanKaps@users.noreply.github.com>
2019-11-02 19:20:54 +0400

    do not merge

--- test_server/server.js
+++ test_server/server.js
@@ -10,6 +10,6 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 
-var server     =    app.listen(3000,function(){
+var port_number = app.listen(process.env.PORT || 3000);
+app.listen(port_number);
 console.log("Express is running on port 3000");
-});
