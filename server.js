4f374b4fd9609616a631d4c1cf65c0b5f39ee633
HasanKaps <40915487+HasanKaps@users.noreply.github.com>
2019-11-04 20:53:25 +0400

    changes made to serverz

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express    =    require('express');
 const bparser = require('body-parser');
 const app        =    express();
 const path = require('path');
+var mysql = require('mysql');
 
 require('./routes/main')(app);
 app.use(express.static(path.join(__dirname, '/public')));
