fc19ee2433c5c00382498b2d8ed3796d7e19168c
HasanKaps <40915487+HasanKaps@users.noreply.github.com>
2019-11-05 09:47:51 +0400

    all files updated

--- routes/main.js
+++ routes/main.js
@@ -13,6 +13,10 @@ module.exports = function(app)
      app.get('/login',function(req,res){
         res.render('login.html');
     });
+     
+    app.get('/signup',function(req,res){
+      res.render('signup.html');
+    });
     
     
 }
