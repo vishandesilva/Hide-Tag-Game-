6eaa3e205d812883ad4c97a9e5a0fed55591ebaa
HasanKaps <40915487+HasanKaps@users.noreply.github.com>
2019-11-05 09:22:41 +0400

    server updates #2

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,4 @@
-(function() { "use strict";
-
-  const sprSize = 32;
+const sprSize = 32;
 
   var Animation = function(frame_set, delay) {
 
@@ -44,6 +42,12 @@
 
   buffer = document.createElement("canvas").getContext("2d");
   display = document.querySelector("canvas").getContext("2d");
+  var bgImage = new Image();
+  bgImage.src = '../media/testmap.png';
+  var obstacle = new Image();
+  obstacle.src = '../media/obstacle.png';
+  //bgImage.src = '../media/map_test.png';
+  
 
   controller = {
 
@@ -96,12 +100,12 @@
   player = {// defining dimensions and required variables for movement
 
     animation:new Animation(),
-    height:32,
-    width:32,
-    x:0,
-    y:0,
-    x_velocity:0,
-    y_velocity:0
+    height:60,
+    width:60,
+    x:Math.floor(Math.random()*buffer.canvas.width),
+    y:Math.floor(Math.random()*buffer.canvas.height),
+    //x_velocity:0,
+    //y_velocity:0
   };
 
   sprite_sheet = {// defining the number of frames in the srite
@@ -115,65 +119,81 @@
 
     if (controller.up.active) {// move character up
 
-      player.animation.change(sprite_sheet.frame_sets[0], 15);
-      player.y_velocity -= 0.2;
+      player.animation.change(sprite_sheet.frame_sets[0], 10);
+      player.y -= 3;
     }
 
     if (controller.left.active) {// move character left
 
-      player.animation.change(sprite_sheet.frame_sets[1], 15);
-      player.x_velocity -= 0.2;
+      player.animation.change(sprite_sheet.frame_sets[1], 10);
+      player.x -= 3;
     }
 
     if (controller.right.active) {// move character right
 
-      player.animation.change(sprite_sheet.frame_sets[3], 15);
-      player.x_velocity += 0.2;
+      player.animation.change(sprite_sheet.frame_sets[3], 10);
+      player.x += 3;
     }
 
     if (controller.down.active) {// move character down
 
-        player.animation.change(sprite_sheet.frame_sets[2], 15);
-        player.y_velocity += 0.2;
+        player.animation.change(sprite_sheet.frame_sets[2], 10);
+        player.y += 3;
       }
 
     if (!controller.left.active && !controller.right.active && !controller.up.active && !controller.down.active) {// when the character is still
 
-      player.animation.change(sprite_sheet.frame_sets[4], 15);
+      player.animation.change(sprite_sheet.frame_sets[4], 10);
     }
 
     // changing the coordinates according to movement speed
-    player.x += player.x_velocity;
-    player.y += player.y_velocity;
-    player.x_velocity *= 0.91;
-    player.y_velocity *= 0.91;
+    //player.x += player.x_velocity;
+    //player.y += player.y_velocity;
+    //player.x_velocity *=0.91;
+    //player.y_velocity *=0.91;
 
     // restricting movement to canvas to avoid overflow
-    if (player.x <= 0) {
-      player.x = 0;
+    if (player.x <= 28) {
+        if(player.y <= (buffer.canvas.height/2)+20 && player.y >=(buffer.canvas.height/2)-60){
+            if(player.x <=0){
+              player.x = Math.floor(Math.random()*buffer.canvas.width);
+              player.y = Math.floor(Math.random()*buffer.canvas.height);
+            }
+        }
+        else{ player.x = 28;}
     }
     else if (player.x + player.width >= buffer.canvas.width) {
-      player.x = buffer.canvas.width - player.width;
+        if(player.y <= (buffer.canvas.height/2)+20 && player.y >= (buffer.canvas.height/2)-60){
+            if (player.x >= buffer.canvas.width - (player.width)/2) {
+              player.x = Math.floor(Math.random()*buffer.canvas.width);
+              player.y = Math.floor(Math.random()*buffer.canvas.height);
+            }
+        }
+        else{
+            player.x = buffer.canvas.width - player.width;
+        }
     }
 
-    if (player.y <= 0) {
-        player.y = 0;
+    if (player.y <= 28) {
+        player.y = 28;
       }
       else if (player.y + player.height >= buffer.canvas.height) {
         player.y = buffer.canvas.height - player.height;
       }
-  
+    
     player.animation.update();
     render();
     window.requestAnimationFrame(loop);
   };
 
   render = function() {
-
-    buffer.fillStyle = "#FFF9FA";
-    buffer.fillRect(0, 0, buffer.canvas.width, buffer.canvas.height);
+    buffer.drawImage(bgImage,0,0,buffer.canvas.width,buffer.canvas.height);
+    //buffer.fillStyle = "black";
+    //buffer.fillRect(0, 0, buffer.canvas.width, buffer.canvas.height);
     buffer.drawImage(sprite_sheet.image, player.animation.frame * sprSize, 0, sprSize, sprSize, Math.floor(player.x), Math.floor(player.y), sprSize, sprSize);
+   // buffer.drawImage(obstacle,(buffer.canvas.width/2),(buffer.canvas.height/2),10,100);
     display.drawImage(buffer.canvas, 0, 0, buffer.canvas.width, buffer.canvas.height, 0, 0, display.canvas.width, display.canvas.height);
+    
   };
 
   resize = function() {
@@ -184,12 +204,13 @@
       display.canvas.width = document.documentElement.clientHeight;
     }
 
-    display.canvas.height = display.canvas.width * 0.5;// keeps canvas length to width ratio the same
+    canvas.width = window.innerWidth - 30;
+canvas.height = (window.innerWidth - 30) * 0.5;// keeps canvas length to width ratio the same
     display.imageSmoothingEnabled = false;// keeps the character pixelated
   };
 
-  buffer.canvas.width = 300;
-  buffer.canvas.height = 200;
+  buffer.canvas.width = 900;
+  buffer.canvas.height = 600;
 
   window.addEventListener("resize", resize);
   window.addEventListener("keydown", controller.keyUpDown);
@@ -202,5 +223,15 @@
     window.requestAnimationFrame(loop);
   });
 
-  sprite_sheet.image.src = "../main/media/sprites/Final Sprite.png";
-})();
+sprite_sheet.image.src = "../media/sprites/Final Sprite.png";  
+
+
+
+document.getElementById("canvas").style.display="none";
+function myFunction()
+{
+   document.getElementById("canvas").style.display="block";
+   document.getElementById("gamebutton").style.display="none";
+   document.getElementById("header").style.display="none";
+   document.getElementById("footer").style.display="none";
+}
