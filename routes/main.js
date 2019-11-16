3135e17f4dcb1aa0349fcb547c3a733a5297c43d
HasanKaps <40915487+HasanKaps@users.noreply.github.com>
2019-11-16 22:49:27 +0400

    Server Database update

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,12 @@
 module.exports = function(app)
 {
      app.get('/',function(req,res){
-        res.render('index.html')
+      res.set({
+         'Access-Control-Allow-Origin' : '*'
+      });
+      return res.render('index.html');
      });
+
      app.get('/about',function(req,res){
         res.render('about.html');
     });
@@ -14,9 +18,61 @@ module.exports = function(app)
         res.render('login.html');
     });
      
-    app.get('/signup',function(req,res){
-      res.render('signup.html');
-    });
+    app.route('/signup')
+       .get((req,res) => {
+          res.render('signup.html');
+       })
+
+       .post((req, res) => {
+         var mongo = require('mongodb');
+         var crypto = require('crypto');
+         var new_db = "mongodb://localhost/database_name";
+
+         var getHash = ( pass , phone ) => {
+				
+            var hmac = crypto.createHmac('sha512', phone);
+            
+            //passing the data to be hashed
+            data = hmac.update(pass);
+            //Creating the hmac in the required format
+            gen_hmac= data.digest('hex');
+            //Printing the output on the console
+            console.log("hmac : " + gen_hmac);
+            return gen_hmac;
+         }
+
+          var name = req.body.name;
+          var pass = req.body.password;
+      
+          var password = getHash( pass , name); 				
+      
+         
+          var data = {
+            "name":name,	
+            "password": password
+            
+         }
+         
+         mongo.connect(new_db , function(error , db){
+            if (error){
+               throw error;
+            }
+            console.log("connected to database successfully");
+            //CREATING A COLLECTION IN MONGODB USING NODE.JS
+            db.collection("details").insertOne(data, (err , collection) => {
+               if(err) throw err;
+               console.log("Record inserted successfully");
+               console.log(collection);
+            });
+         });
+         
+         console.log("DATA is " + JSON.stringify(data) );
+         res.set({
+            'Access-Control-Allow-Origin' : '*'
+         });
+         return res.render('index.html');  
+      
+      });
     
     
 }
--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express    =    require('express');
-const bparser = require('body-parser');
-const app        =    express();
+const bodyParser = require('body-parser');
+const app = express();
 const path = require('path');
-var mysql = require('mysql');
 
+
+//enter the name of the database in the end 
+
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+	extended: true
+}));
 require('./routes/main')(app);
 app.use(express.static(path.join(__dirname, '/public')));
+
 app.set('views',__dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-
 var port_number = app.listen(process.env.PORT || 3000);
 app.listen(port_number);
 console.log("Express is running");
+
+
+ 
