95e7dda522bf1bbe2b7d38f1aae1b2fc5adb1eab
HasanKaps <40915487+HasanKaps@users.noreply.github.com>
2019-11-23 16:27:34 +0400

    server db

--- routes/main.js
+++ routes/main.js
@@ -14,14 +14,31 @@ module.exports = function(app)
     app.get('/leaderboards',function(req,res){
       res.render('leaderboards.html');
   });
-   
-  app.get('/register',function(req,res){
-   res.render('register.html');
-});
 
     app.route('/registration')
        .get((req,res) => {
        res.render('register.html')
     })
- }
+       .post((req,res) => {
+         var username = request.body.username;
+         var password = request.body.password;
+         if (username && password) {
+            connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+               if (results.length > 0) {
+                  request.session.loggedin = true;
+                  request.session.username = username;
+                  response.redirect('/home');
+               } else {
+                  response.send('Incorrect Username and/or Password!');
+               }			
+               response.end();
+            });
+         } else {
+            response.send('Please enter Username and Password!');
+            response.end();
+         }
+      });
+      
+       
+ } 
  
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
 	password : '',
-	database : 'nodelogin'
+	database : 'login.sql'
 });
 
 var app = express();
