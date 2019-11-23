041a33cd15e4907f1a62bf5e042574e6fef16814
HasanKaps <40915487+HasanKaps@users.noreply.github.com>
2019-11-23 14:00:00 +0400

    few changes made

--- routes/main.js
+++ routes/main.js
@@ -7,82 +7,13 @@ module.exports = function(app)
       return res.render('index.php');
      });
 
-     /* app.get('/about',function(req,res){
-        res.render('about.html');
-    });
-     app.get('/leaderboards',function(req,res){
-        res.render('leaderboards.html');
-    }); */
-
      app.get('/login',function(req,res){
         res.render('login.php');
     });
      
-    app.get('/registration',function(req,res){
-      res.render('register.php');
-    });
-     
- }
-   /*   app.route('/signup')
+    app.route('/registration')
        .get((req,res) => {
-       res.render('signup.html');
+       res.render('register.php')
     })
-
-       .post((req, res) => {
-       var mongo = require('mongodb');
-       var crypto = require('crypto');
-       var new_db = "mongodb://localhost/database_name";
-
-       var getHash = ( pass , phone ) => {
-         
-         var hmac = crypto.createHmac('sha512', phone);
-         
-         //passing the data to be hashed
-         data = hmac.update(pass);
-         //Creating the hmac in the required format
-         gen_hmac= data.digest('hex');
-         //Printing the output on the console
-         console.log("hmac : " + gen_hmac);
-         return gen_hmac;
-      }
-
-       var name = req.body.name;
-       var pass = req.body.password;
-   
-       var password = getHash( pass , name); 				
-   
-      
-       var data = {
-         "name":name,	
-         "password": password
-         
-      }
-      
-      mongo.connect(new_db , function(error , db){
-         db.collection('Persons', function (err, collection) {
-        
-            collection.find().toArray(function(err, items) {
-               if(err) throw err;    
-               console.log(items);            
-           });
-           
-       });
-         console.log("connected to database successfully");
-         //CREATING A COLLECTION IN MONGODB USING NODE.JS
-         db.collection("details").insertOne(data, (err , collection) => {
-            if(err) throw err;
-            console.log("Record inserted successfully");
-            console.log(collection);
-         });
-      });
-      
-      console.log("DATA is " + JSON.stringify(data) );
-      res.set({
-         'Access-Control-Allow-Origin' : '*'
-      });
-      return res.render('index.html');  
-   
-   });
+ }
  
- 
-} */
