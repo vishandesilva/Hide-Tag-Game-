dcc38dfeda0f59627d751f1c85fe54a4309d4620
HasanKaps <40915487+HasanKaps@users.noreply.github.com>
2019-11-23 23:47:50 +0400

    db

--- routes/main.js
+++ routes/main.js
@@ -18,71 +18,70 @@ module.exports = function(app)
         res.render('login.html');
     });
      
-    app.route('/register')
-       .get((req,res) => {
-          res.render('register.html');
-       })
+    app.route('/signup')
+    .get((req,res) => {
+       res.render('signup.html');
+    })
 
-       .post((req, res) => {
-         var mongo = require('mongodb');
-         var crypto = require('crypto');
-         var new_db = "mongodb://localhost/database_name";
+    .post((req, res) => {
+      var mongo = require('mongodb');
+      var crypto = require('crypto');
+      var new_db = "mongodb://localhost/database_name";
 
-         var getHash = ( pass , name ) => {
-				
-            var hmac = crypto.createHmac('sha512', name);
+      var getHash = ( pass , name ) => {
+         
+         var hmac = crypto.createHmac('sha512', name);
+
+         data = hmac.update(pass);
+         gen_hmac= data.digest('hex');
+         console.log("hmac : " + gen_hmac);
+         return gen_hmac;
+      }
+
+       var name = req.body.name;
+       var pass = req.body.password;
+
+   //     var fs = require('fs')
+
+   //     fs.readFile(index-login.html, 'utf8', function (err,data) {
+   //       if (err) {
+   //         return console.log(err);
+   //       }
+
+   //     var username = data.replace(/<li><a class="User"><li>/, '<div>' + name +'</div>');
+
+   //     fs.writeFile(index-login.html, username, 'utf8' , function (err) {
+   //       if (err) return console.log(err);
+   //    });
+   //  });
+
+
+       var password = getHash( pass , name); 				
    
-            data = hmac.update(pass);
-            gen_hmac= data.digest('hex');
-            console.log("hmac : " + gen_hmac);
-            return gen_hmac;
-         }
-
-          var name = req.body.name;
-          var pass = req.body.password;
-
-      //     var fs = require('fs')
-
-      //     fs.readFile(index-login.html, 'utf8', function (err,data) {
-      //       if (err) {
-      //         return console.log(err);
-      //       }
-
-      //     var username = data.replace(/<li><a class="User"><li>/, '<div>' + name +'</div>');
-
-      //     fs.writeFile(index-login.html, username, 'utf8' , function (err) {
-      //       if (err) return console.log(err);
-      //    });
-      //  });
-
-
-          var password = getHash( pass , name); 				
       
+       var data = {
+         "name":name,	
+         "password": password
          
-          var data = {
-            "name":name,	
-            "password": password
-            
-         }
-         
-         mongo.connect(new_db , function(error , db){
-            if (error){
-               throw error;
-            }
-            console.log("connected to database successfully");
-            db.collection("details").insertOne(data, (err , collection) => {
-               if(err) throw err;
-               console.log("Record inserted successfully");
-               console.log(collection);
-            });
-         });
-         
-         console.log("DATA is " + JSON.stringify(data) );
-         res.set({
-            'Access-Control-Allow-Origin' : '*'
-         });
-         return res.render('index.html');  
+      }
       
+      mongo.connect(new_db , function(error , db){
+         if (error){
+            throw error;
+         }
+         console.log("connected to database successfully");
+         db.collection("details").insertOne(data, (err , collection) => {
+            if(err) throw err;
+            console.log("Record inserted successfully");
+            console.log(collection);
+         });
       });
-    
+      
+      console.log("DATA is " + JSON.stringify(data) );
+      res.set({
+         'Access-Control-Allow-Origin' : '*'
+      });
+      return res.render('index-login.html');  
+   
+   });
 }
