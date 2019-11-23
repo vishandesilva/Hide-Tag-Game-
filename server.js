a9c4078b735e0aa123fe55e9f8e167f26439c5a6
HasanKaps <40915487+HasanKaps@users.noreply.github.com>
2019-11-23 23:04:53 +0400

    minor changes

--- server.js
+++ server.js
@@ -5,38 +5,6 @@ const mysql = require('mysql');
 const session = require('express-session');
 var app = express();
 
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'password',
-    database : 'wpdb'
-});
-
-connection.connect(function(err) {
-    // in case of error
-    if(err){
-        console.log(err.code);
-        console.log(err.fatal);
-    }
-});
-
-// Perform a query
-$query = 'SELECT * from MyTable LIMIT 10';
-
-connection.query($query, function(err, rows, fields) {
-    if(err){
-        console.log("An error ocurred performing the query.");
-        return;
-    }
-
-    console.log("Query succesfully executed: ", rows);
-});
-
-// Close the connection
-connection.end(function(){
-    // The connection has been closed
-});
-
 app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
 	extended: true
