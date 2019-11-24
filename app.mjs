45c7543c09289c399809ce69304bc89e50af61a8
HasanKaps <40915487+HasanKaps@users.noreply.github.com>
2019-11-24 21:08:37 +0400

    things

--- app.mjs
+++ app.mjs
@@ -18,7 +18,58 @@ app.use(express.static(__dirname + "/"));
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 app.get("/leaderboards", (req, res) => res.sendFile(__dirname + "/leaderboards.html"));
-app.get("/signup", (req, res) => res.sendFile(__dirname + "/signup.html"));
+
+app.route('/signup')
+.get((req,res) => {
+   res.sendFile(__dirname + "/signup.html");
+})
+
+/* .post((req, res) => {
+  var mongo = require('mongodb');
+  var crypto = require('crypto');
+  var user_db = "mongodb://localhost/userdb";
+
+  var getHash = ( pass , name ) => {
+     
+     var hmac = crypto.createHmac('sha512', name);
+
+     data = hmac.update(pass);
+     gen_hmac= data.digest('hex');
+     console.log("hmac : " + gen_hmac);
+     return gen_hmac;
+  }
+
+   var NAME = req.body.name;
+   var pass = req.body.password;
+   var password = getHash( pass , NAME); 				
+
+  
+   var data = {
+     "name":NAME,	
+     "password": password
+     
+  }
+  
+  mongo.connect(user_db , function(error , db){
+     if (error){
+        throw error;
+     }
+     console.log("connected to database successfully");
+     db.collection("users").insertOne(data, (err , collection) => {
+        if(err) throw err;
+        console.log("Record inserted successfully");
+        console.log(collection);
+     });
+  });
+  
+  console.log("DATA is " + JSON.stringify(data) );
+  res.set({
+     'Access-Control-Allow-Origin' : '*'
+  });
+  return res.render('index-login.html');  
+
+});
+ */
 import Coin from "./js/coin.mjs"; 
  
 let players = []; 
